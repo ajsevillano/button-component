cb7edd5c5344ea946c6de2e4eaf2fc5bf96b62e4
Sevi <correosevi@gmail.com>
2021-04-16 20:51:24 +0100

    Add material icons font to the button

--- src/components/Button/Index.js
+++ src/components/Button/Index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Button = ({ variant, disabled, boxShadow, color }) => {
   return (
-    <div>
-      <button
-        className={`btn ${color} ${boxShadow ? 'box-shadow' : ''} ${variant}`}
-        disabled={disabled}
-      >
-        Default
-      </button>
-    </div>
+    <button
+      className={`btn ${color} ${boxShadow ? 'box-shadow' : ''} ${variant}`}
+      disabled={disabled}
+    >
+      <span class="material-icons">face</span>
+      <p>Default</p>
+      <span class="material-icons">face</span>
+    </button>
   );
 };
 
