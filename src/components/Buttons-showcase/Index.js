2f11bdb5f4976cd288591cef75cac14826f5024e
Sevi <correosevi@gmail.com>
2021-04-20 15:40:16 +0100

    Add margin to the headline

--- src/components/Buttons-showcase/Index.js
+++ src/components/Buttons-showcase/Index.js
@@ -5,31 +5,31 @@ const ButtonsShowcase = () => {
   return (
     <div className="buttons-display-container">
       <div className="button-container" id="default-button">
-        <p>{buttonHeaders.default}</p>
+        <p className="headline">{buttonHeaders.default}</p>
         <Button variant="default" color="Default" boxShadow={true} size="sm" />
       </div>
       <div className="button-container" id="outline-button">
-        <p>{buttonHeaders.outline}</p>
+        <p className="headline">{buttonHeaders.outline}</p>
         <Button variant="outline" size="sm" />
       </div>
       <div className="button-container" id="text-button">
-        <p>{buttonHeaders.text}</p>
+        <p className="headline">{buttonHeaders.text}</p>
         <Button variant="text" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.disableshadow}</p>
+        <p className="headline">{buttonHeaders.disableshadow}</p>
         <Button variant="default" color="Primary" size="sm" />
       </div>
       <div className="button-container" id="disabled-button">
-        <p>{buttonHeaders.disabled}</p>
+        <p className="headline">{buttonHeaders.disabled}</p>
         <Button variant="default" disabled={true} size="sm" />
       </div>
       <div className="button-container" id="disabled-button">
-        <p>{buttonHeaders.textDisabled}</p>
+        <p className="headline">{buttonHeaders.textDisabled}</p>
         <Button variant="text" disabled={true} />
       </div>
       <div className="button-container" id="default-button">
-        <p>{buttonHeaders.startIcon}</p>
+        <p className="headline">{buttonHeaders.startIcon}</p>
         <Button
           variant="default"
           color="Primary"
@@ -38,7 +38,7 @@ const ButtonsShowcase = () => {
         />
       </div>
       <div className="button-container" id="default-button">
-        <p>{buttonHeaders.endIcon}</p>
+        <p className="headline">{buttonHeaders.endIcon}</p>
         <Button
           variant="default"
           color="Primary"
@@ -47,31 +47,31 @@ const ButtonsShowcase = () => {
         />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.sm}</p>
+        <p className="headline">{buttonHeaders.sm}</p>
         <Button variant="default" color="Primary" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.md}</p>
+        <p className="headline">{buttonHeaders.md}</p>
         <Button variant="default" color="Primary" size="md" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.lg}</p>
+        <p className="headline">{buttonHeaders.lg}</p>
         <Button variant="default" color="Primary" size="lg" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.colorDefault}</p>
+        <p className="headline">{buttonHeaders.colorDefault}</p>
         <Button variant="default" color="Default" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.colorPrimary}</p>
+        <p className="headline">{buttonHeaders.colorPrimary}</p>
         <Button variant="default" color="Primary" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.colorSecundary}</p>
+        <p className="headline">{buttonHeaders.colorSecundary}</p>
         <Button variant="default" color="Secundary" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
-        <p>{buttonHeaders.colorDanger}</p>
+        <p className="headline">{buttonHeaders.colorDanger}</p>
         <Button variant="default" color="Danger" size="sm" />
       </div>
       <div className="button-container" id="disableShadow-button">
